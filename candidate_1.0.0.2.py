9b11ce9499c662c3ed60042c45c8c2791ef06cbd
Eric Scuccimarra <skooch@gmail.com>
2018-04-09 13:54:36 +0200

    Debugging

--- candidate_1.0.0.2.py
+++ candidate_1.0.0.2.py
@@ -59,7 +59,7 @@ graph = tf.Graph()
 
 # whether to retrain model from scratch or use saved model
 init = True
-model_name = "model_s1.0.0.16b"
+model_name = "model_s1.0.0.17b"
 # 0.0.0.4 - increase pool3 to 3x3 with stride 3
 # 0.0.0.6 - reduce pool 3 stride back to 2
 # 0.0.0.7 - reduce lambda for l2 reg
@@ -649,7 +649,7 @@ with tf.Session(graph=graph, config=config) as sess:
         # evaluate the test data
         for X_batch, y_batch in get_batches(X_cv, y_cv, batch_size, distort=False):
             _, _, valid_acc, valid_recall, valid_precision, valid_fscore, valid_cost = sess.run(
-                [extra_update_ops, update_op, accuracy, rec_op, prec_op, f1_score, mean_ce],
+                [update_op, extra_update_ops, accuracy, rec_op, prec_op, f1_score, mean_ce],
                 feed_dict={
                     X: X_batch,
                     y: y_batch,
@@ -668,8 +668,8 @@ with tf.Session(graph=graph, config=config) as sess:
         # Write average of validation data to summary logs
         if log_to_tensorboard:
             # evaluate once more to get the summary, which will then be written to tensorboard
-            _, summary, valid_acc, valid_recall, valid_precision, valid_fscore, valid_cost = sess.run(
-                [extra_update_ops, test_merged, accuracy, rec_op, prec_op, f1_score, mean_ce],
+            summary, valid_acc, valid_recall, valid_precision, valid_fscore, valid_cost = sess.run(
+                [test_merged, accuracy, rec_op, prec_op, f1_score, mean_ce],
                 feed_dict={
                     X: X_cv[0:2],
                     y: y_cv[0:2],
