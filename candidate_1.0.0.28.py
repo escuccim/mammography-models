9f142c7c9e092207c3431063b814308b5b3f678e
Eric Scuccimarra <skooch@gmail.com>
2018-04-10 10:51:03 +0200

    Debugging

--- candidate_1.0.0.28.py
+++ candidate_1.0.0.28.py
@@ -583,7 +583,7 @@ with graph.as_default():
         labels=y,
         predictions=predictions,
         updates_collections=tf.GraphKeys.UPDATE_OPS,
-        metrics_collections=["summaries"],
+        #metrics_collections=["summaries"],
         name="accuracy",
     )
 
