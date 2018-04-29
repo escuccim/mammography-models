c9e4a516c02e78ae9c8b0021e73c3bd688d7672e
Eric Scuccimarra <skooch@gmail.com>
2018-04-29 11:13:38 +0200

    Evaluating 45 on 9l

--- candidate_1.0.0.42.py
+++ candidate_1.0.0.42.py
@@ -75,7 +75,7 @@ print("Number of classes:", num_classes)
 ## Build the graph
 graph = tf.Graph()
 
-model_name = "model_s1.0.0.45b.8.1"
+model_name = "model_s1.0.0.45l.9.1"
 ## Change Log
 # 0.0.0.4 - increase pool3 to 3x3 with stride 3
 # 0.0.0.6 - reduce pool 3 stride back to 2
