Trying to fix scaling problems?

candidate_1.0.0.28.2.py
@@ -81,7 +81,7 @@ print("Number of classes:", num_classes)
 ## Build the graph
 graph = tf.Graph()
 
-model_name = "model_s1.0.0.34b.8"
+model_name = "model_s1.0.0.35b.9"
 ## Change Log
 # 0.0.0.4 - increase pool3 to 3x3 with stride 3
 # 0.0.0.6 - reduce pool 3 stride back to 2
@@ -134,7 +134,7 @@ with graph.as_default():
         y = tf.placeholder_with_default(y_def, shape=[None])
 
         #X = tf.cast(X, dtype=tf.float32)
-        X_adj = _scale_input_data(X, contrast=contrast, mu=127.0)
+        X_adj = _scale_input_data(X, contrast=contrast, mu=0, scale=255.0)
 
     # Convolutional layer 1
     with tf.name_scope('conv1') as scope:

training_utils.py
@@ -547,7 +547,7 @@ def flatten(l):
             out.append(item)
     return out
 
-def _scale_input_data(X, contrast=None, mu=104.1353):
+def _scale_input_data(X, contrast=None, mu=104.1353, scale=255.0):
     # if we are adjusting contrast do that
     if contrast:
         X_adj = tf.image.adjust_contrast(X, contrast)
@@ -562,6 +562,6 @@ def _scale_input_data(X, contrast=None, mu=104.1353):
     X_adj = tf.subtract(X_adj, mu_tf, name="centered_input")
 
     # scale the data
-    X_adj = tf.divide(X_adj, 255.0)
+    X_adj = tf.divide(X_adj, scale)
 
     return X_adj