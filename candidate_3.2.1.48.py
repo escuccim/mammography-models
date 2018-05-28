3fba2c32025a2af33ac5fce288b6698128167326
Eric Scuccimarra <skooch@gmail.com>
2018-05-28 10:36:25 +0200

    Changing size of inputs2

--- candidate_3.2.1.48.py
+++ candidate_3.2.1.48.py
@@ -169,7 +169,7 @@ with graph.as_default():
 
     with tf.name_scope('inputs') as scope:
         with tf.device('/cpu:0'):
-            image, label = read_and_decode_single_example(train_files, label_type=how, normalize=False, distort=False, size=320)
+            image, label = read_and_decode_single_example(train_files, label_type=how, normalize=False, distort=False, size=640)
 
             X_def, y_def = tf.train.shuffle_batch([image, label], batch_size=batch_size, capacity=2000, seed=None, min_after_dequeue=1000)
 
