8c20b1d8ee69a7240be42325874a8db5f2dd8a95
Eric Scuccimarra <skooch@gmail.com>
2018-08-27 15:31:24 +0200

    Testing IOU loss

--- candidate_3.9.4.01.py
+++ candidate_3.9.4.01.py
@@ -1144,7 +1144,7 @@ with graph.as_default():
         labels_fl = tf.cast(y_adj, tf.float32)
         inter = tf.reduce_sum(tf.multiply(logits, labels_fl))
         union = tf.reduce_sum(tf.sub(tf.add(logits, labels_fl), tf.multiply(logits, labels_fl)))
-        loss = tf.sub(tf.constant(1.0, dtype=tf.float32), tf.div(inter, union)) + tf.losses.get_regularization_loss()
+        loss = tf.subtract(tf.constant(1.0, dtype=tf.float32), tf.div(inter, union)) + tf.losses.get_regularization_loss()
     else:
         mean_ce = tf.reduce_mean(tf.losses.sigmoid_cross_entropy(multi_class_labels=y_adj, logits=logits_sm, weights=weights))
 
