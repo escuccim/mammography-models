1f727f94c4c6d42d68e6678d0fe0e5d3e1b4cba4
Eric Scuccimarra <skooch@gmail.com>
2018-09-03 18:13:10 +0200

    Moving data augmentation elsewhere to try to speed up training

--- candidate_3.9.4.02.py
+++ candidate_3.9.4.02.py
@@ -213,7 +213,7 @@ with graph.as_default():
         with tf.device('/cpu:0'):
             if dataset == 100:
                 # decode the image
-                image, label = _read_images("./data/train_images/", size, scale_by=0.66, distort=distort,
+                image, label = _read_images("./data/train_images/", size, scale_by=0.66, distort=False,
                                             standardize=normalize)
             else:
                 image, label = read_and_decode_single_example(train_files, label_type=how, normalize=False,
